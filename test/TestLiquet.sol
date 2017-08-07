937475d83f46eea38732170795763be60cc3f683
pz7 <pz7@users.noreply.github.com>
2017-08-07 15:27:36 -0700

    Update TestLiquet.sol

--- test/TestLiquet.sol
+++ test/TestLiquet.sol
@@ -13,15 +13,4 @@ contract TestLiquet {
 
     Assert.equal(liquet.balanceOf(msg.sender), expected, "Owner should have 10,000,000 Liquets initially");
   }
-
-  function testTransferUsingDeployedContract() {
-    Liquet liquet = Liquet(DeployedAddresses.Liquet());
-
-    uint value = 10;
-
-    bool success = liquet.transfer(0x1, value);
-
-    Assert.isTrue(success, "Trasfer should be successful");
-    Assert.equal(liquet.balanceOf(0x1), value, "Address should have 10 Liquets after transfer");
-  }
 }
