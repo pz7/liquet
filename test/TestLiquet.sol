a5956568c6c849b2bcb47030d9bf87a585a51879
pz7 <pz7@users.noreply.github.com>
2017-08-07 06:50:22 -0700

    Update TestLiquet.sol

--- test/TestLiquet.sol
+++ test/TestLiquet.sol
@@ -9,7 +9,7 @@ contract TestLiquet {
   function testInitialBalanceUsingDeployedContract() {
     Liquet liquet = Liquet(DeployedAddresses.Liquet());
 
-    uint expected = 100000000;
+    uint expected = 10000000;
 
     Assert.equal(liquet.balanceOf(tx.origin), expected, "Owner should have 100,000,000 MetaCoin initially");
   }
@@ -17,7 +17,7 @@ contract TestLiquet {
   function testInitialBalanceWithNewLiquet() {
     Liquet liquet = new Liquet();
 
-    uint expected = 100000000;
+    uint expected = 10000000;
 
     Assert.equal(liquet.balanceOf(tx.origin), expected, "Owner should have 100,000,000 MetaCoin initially");
   }
