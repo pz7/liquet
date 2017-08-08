c77b784323395078179c9edd995ce717943e5a83
pz7 <pz7@users.noreply.github.com>
2017-08-08 06:29:11 -0700

    Create liquet.js

---
+++ test/liquet.js
@@ -0,0 +1,47 @@
+// Specifically request an abstraction for Liquet
+var Liquet = artifacts.require("Liquet");
+
+contract('Liquet', function(accounts) {
+  it("should put 100,000,000 Liquets in the first account", function() {
+    return Liquet.deployed().then(function(instance) {
+      return instance.getBalance.call(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance.valueOf(), 100000000, "100000000 wasn't in the first account");
+    });
+  });
+  it("should send coin correctly", function() {
+    var liquet;
+
+    // Get initial balances of first and second account.
+    var account_one = accounts[0];
+    var account_two = accounts[1];
+
+    var account_one_starting_balance;
+    var account_two_starting_balance;
+    var account_one_ending_balance;
+    var account_two_ending_balance;
+
+    var amount = 10;
+
+    return Liquet.deployed().then(function(instance) {
+      liquet = instance;
+      return liquet.getBalance.call(account_one);
+    }).then(function(balance) {
+      account_one_starting_balance = balance.toNumber();
+      return liquet.getBalance.call(account_two);
+    }).then(function(balance) {
+      account_two_starting_balance = balance.toNumber();
+      return liquet.sendCoin(account_two, amount, {from: account_one});
+    }).then(function() {
+      return liquet.getBalance.call(account_one);
+    }).then(function(balance) {
+      account_one_ending_balance = balance.toNumber();
+      return liquet.getBalance.call(account_two);
+    }).then(function(balance) {
+      account_two_ending_balance = balance.toNumber();
+
+      assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
+      assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
+    });
+  });
+});
