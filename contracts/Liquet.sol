646576e9d40a6855de97e6128e2f015d1c9d568c
pz7 <pz7@users.noreply.github.com>
2017-08-08 05:39:32 -0700

    Update Liquet.sol

--- contracts/Liquet.sol
+++ contracts/Liquet.sol
@@ -3,61 +3,6 @@ pragma solidity ^0.4.13;
 import '../zeppelin-solidity/contracts/token/StandardToken.sol';
 import '../zeppelin-solidity/contracts/ownership/Claimable.sol';   
 
-contract Token {
-    uint public totalSupply;
-    function balanceOf(address _owner) constant returns (uint balance);
-    function transfer(address _to, uint _value) returns (bool success);
-    function transferFrom(address _from, address _to, uint _value) returns (bool success);
-    function approve(address _spender, uint _value) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint remaining);
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-}
-
-/*  ERC 20 token */
-contract StandardToken is Token {
-
-    mapping (address => uint) balances;
-    mapping (address => mapping (address => uint)) allowed;
-
-    function transfer(address _to, uint _value) returns (bool success) {
-      if (balances[msg.sender] >= _value && _value > 0) {
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        Transfer(msg.sender, _to, _value);
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-    function transferFrom(address _from, address _to, uint _value) returns (bool success) {
-      if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        allowed[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-    function balanceOf(address _owner) constant returns (uint balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint remaining) {
-      return allowed[_owner][_spender];
-    }
-}
-
 contract Liquet is StandardToken {
     string public constant name = "Liquet";
     string public constant symbol = "LQT";
