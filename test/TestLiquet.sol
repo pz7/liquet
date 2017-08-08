c5f01de75b1a8e138fed9e6e7fc485fc38e4588c
pz7 <pz7@users.noreply.github.com>
2017-08-08 06:17:17 -0700

    Update TestLiquet.sol

--- test/TestLiquet.sol
+++ test/TestLiquet.sol
@@ -17,7 +17,7 @@ contract TestLiquet {
   function testNameUsingDeployedContract() {
     Liquet liquet = Liquet(DeployedAddresses.Liquet());
 
-    bytes32 name = liquet.name();
+    string name = liquet.name();
 
     Assert.equal(name, "Liquet", "The name should be Liquet");
   }
