808e9608a861e249305085d594a214c533ab1508
pz7 <pz7@users.noreply.github.com>
2017-08-10 09:39:35 -0700

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,5 +1,10 @@
 module.exports = {
   networks: {
+    live: {
+      host: "localhost",
+      port: 8545,
+      network_id: "1"
+    },
     development: {
       host: "localhost",
       port: 8545,
