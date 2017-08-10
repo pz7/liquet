fa2d32c2c66420d371c57c8ff3712db372709a3a
pz7 <pz7@users.noreply.github.com>
2017-08-10 07:59:21 -0700

    Update Liquet.sol

--- contracts/Liquet.sol
+++ contracts/Liquet.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.15;
 
 import '../zeppelin-solidity/contracts/token/StandardToken.sol';
 import '../zeppelin-solidity/contracts/ownership/Claimable.sol';   
@@ -7,7 +7,7 @@ contract Liquet is StandardToken {
     string public constant name = "Liquet";
     string public constant symbol = "LQT";
     uint public constant decimals = 18;
-    uint public constant totalSupply = 10 * (10**6) * (10**decimals);
+    uint public constant totalSupply = 100 * (10**6) * (10**decimals);
     string public constant version = "1.0";
 
     function Liquet() {
