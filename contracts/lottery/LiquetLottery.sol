0c3b5947551eb33577fd8cb8d6c8a24f87f698c0
pz7 <peter.zavadsky@gmail.com>
2017-10-04 21:30:01 -0700

    adding file

---
+++ contracts/lottery/LiquetLottery.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.15;
+
+import 'zeppelin-solidity/contracts/ownership/Claimable.sol';
+
+contract LiquetLottery is Claimable {
+
+    function LiquetLottery() {
+    }
+
+    function() {
+        revert();
+    }
+}
