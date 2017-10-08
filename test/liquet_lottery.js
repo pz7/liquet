957136c44159016ec91bd9352123e351606e79ed
pz7 <peter.zavadsky@gmail.com>
2017-10-07 21:52:23 -0700

    adding context to test

--- test/liquet_lottery.js
+++ test/liquet_lottery.js
@@ -3,18 +3,24 @@ var Liquet = artifacts.require("Liquet");
 var LiquetLottery = artifacts.require("LiquetLottery");
 
 contract('LiquetLottery', function(accounts) {
-  it("should give a status", function() {
+  it("should take a bet", function() {
+    var lottery;
     return LiquetLottery.deployed().then(function(instance) {
+      lottery = instance;
       // TODO
     });
   });
-  it("should take a bet", function() {
+  it("should give a status", function() {
+    var lottery;
     return LiquetLottery.deployed().then(function(instance) {
+      lottery = instance;
       // TODO
     });
   });
   it("should pay the winner(s)", function() {
+    var lottery;
     return LiquetLottery.deployed().then(function(instance) {
+      lottery = instance;
       // TODO
     });
   });
