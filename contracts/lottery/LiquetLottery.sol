09d0aa4325584b6669c0981a1b605d8b84fc3849
pz7 <peter.zavadsky@gmail.com>
2017-10-09 22:42:05 -0700

    adding base call for bet

--- contracts/lottery/LiquetLottery.sol
+++ contracts/lottery/LiquetLottery.sol
@@ -4,7 +4,21 @@ import 'zeppelin-solidity/contracts/ownership/Claimable.sol';
 
 contract LiquetLottery is Claimable {
 
+   struct Bet {
+     uint a;
+     uint b;
+     uint c;
+     uint d;
+     uint e;
+     uint f;
+   }
+
     function LiquetLottery() {
+    }
+
+    function bet(uint a, uint b, uint c, uint d, uint e, uint f) {
+      Bet memory bt = Bet(a, b, c, d, e, f);
+
     }
 
     function() {
--- test/liquet_lottery.js
+++ test/liquet_lottery.js
@@ -3,11 +3,12 @@ var Liquet = artifacts.require("Liquet");
 var LiquetLottery = artifacts.require("LiquetLottery");
 
 contract('LiquetLottery', function(accounts) {
+  var bet = { "a":1, "b":2, "c":3, "d":4, "e":5, "f":6};
   it("should take a bet", function() {
     var lottery;
     return LiquetLottery.deployed().then(function(instance) {
       lottery = instance;
-      // TODO
+      lottery.bet(bet);
     });
   });
   it("should give a status", function() {
